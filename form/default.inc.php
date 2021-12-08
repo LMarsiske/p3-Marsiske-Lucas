c897b1eebe667b269e84321854436e1b99a7523e
Lucas Marsiske <47642996+LMarsiske@users.noreply.github.com>
2021-12-08 15:28:18 -0500

    Added alts and titles to images, working on extras page. Trying to implement lightbox

---
+++ form/default.inc.php
@@ -0,0 +1,32 @@
+<style>
+    ul {
+        list-style-type: none;
+    }
+
+    #home {
+        text-decoration: none;
+        font: 24px "Georama", sans-serif;
+        color: #f87839;
+    }
+
+    h1 {
+        font: 48px "Futurist", sans-serif;
+    }
+
+    p {
+        font: 16px "Georama", sans-serif;
+    }
+</style>
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="index.html" id="home">&#129044; Return to home</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+<div class="container">
+    <h1>Sorry, something went wrong with your submission</h1>
+    <p>Please try again</p>
+</div>
---
+++ form/thankyou.inc.php
@@ -0,0 +1,33 @@
+<style>
+    ul {
+        list-style-type: none;
+    }
+
+    #home {
+        text-decoration: none;
+        font: 24px "Georama", sans-serif;
+        color: #f87839;
+    }
+
+    h1 {
+        font: 48px "Futurist", sans-serif;
+    }
+
+    p {
+        font: 16px "Georama", sans-serif;
+    }
+</style>
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="index.html" id="home">&#129044; Return to home</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+<div class="container">
+    <h1>Thank you for your enquiry</h1>
+    <p>Thanks for your interest. we have received your message, and we will be in touch shortly.</p>
+    <p>Form processor provided by <a href="http://www.inventpartners.com">Invent Partners web design York</a>.</p>
+</div>
